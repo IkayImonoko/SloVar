f2e46173070ef8cf964a19bb529751429f9e8c96
IkayImonoko <ntolok1990@gmail.com>
2026-06-23 15:29:36 +0200

    first commit

---
+++ Domain/Core/Agregats/LearningProgress.cs
@@ -0,0 +1,33 @@
+﻿namespace Domain.Core.Agregats;
+
+public class LearningProgress
+{
+    public Guid LearnerId { get; private set; }
+    public Guid CardId { get; private set; }
+
+    public SrsStage SrsStage { get; private set; }
+
+    public DateTime NextReviewDate { get; private set; }
+
+    public int Streak { get; private set; }
+    public int CorrectCount { get; private set; }
+    public int WrongCount { get; private set; }
+
+    public void RegisterCorrectAnswer(DateTime nextReviewDate)
+    {
+        CorrectCount++;
+        Streak++;
+
+        SrsStage = SrsStage.Next();
+        NextReviewDate = nextReviewDate;
+    }
+
+    public void RegisterWrongAnswer(DateTime nextReviewDate)
+    {
+        WrongCount++;
+        Streak = 0;
+
+        SrsStage = SrsStage.Previous();
+        NextReviewDate = nextReviewDate;
+    }
+}
---
+++ Domain/Core/SrsStage.cs
@@ -0,0 +1,46 @@
+﻿namespace Domain.Core;
+
+public sealed class SrsStage
+{
+    public static readonly SrsStage Apprentice = new(0, "Apprentice");
+    public static readonly SrsStage Guru = new(1, "Guru");
+    public static readonly SrsStage Master = new(2, "Master");
+    public static readonly SrsStage Enlightened = new(3, "Enlightened");
+    public static readonly SrsStage Burned = new(4, "Burned");
+
+    private static readonly SrsStage[] Stages =
+    {
+        Apprentice,
+        Guru,
+        Master,
+        Enlightened,
+        Burned
+    };
+
+    public int Level { get; }
+    public string Name { get; }
+
+    private SrsStage(int level, string name)
+    {
+        Level = level;
+        Name = name;
+    }
+
+    public SrsStage Next()
+    {
+        if (Level == Stages.Length - 1)
+            return this;
+
+        return Stages[Level + 1];
+    }
+
+    public SrsStage Previous()
+    {
+        if (Level == 0)
+            return this;
+
+        return Stages[Level - 1];
+    }
+
+    public override string ToString() => Name;
+}
